9ef8a630d6193e55cd21a9c8c5eb71fa82156d41
nvhung1810 <nguyenvanhung181001@gmail.com>
2024-01-18 10:06:33 +0700

    source base

---
+++ src/main/java/book/be/gau/book_ha/BookHaApplication.java
@@ -0,0 +1,13 @@
+package book.be.gau.book_ha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookHaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookHaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/book/be/gau/book_ha/BookHaApplicationTests.java
@@ -0,0 +1,13 @@
+package book.be.gau.book_ha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookHaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
