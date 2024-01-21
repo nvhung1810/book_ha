605e78925e949d8209cb8a0bf4644a4abcfcfd32
nvhung1810 <nguyenvanhung181001@gmail.com>
2024-01-21 21:54:03 +0700

    feat: check duplicate account

--- src/main/java/book/be/gau/book_ha/repositories/CustomerRepository.java
+++ src/main/java/book/be/gau/book_ha/repositories/CustomerRepository.java
@@ -10,4 +10,7 @@ import book.be.gau.book_ha.models.Customer;
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
   @Query("SELECT c FROM Customer c WHERE c.customer_staff_email = ?1")
   Optional<Customer> findByCustomerStaffEmail(String customerStaffEmail);
+
+  @Query("SELECT CASE WHEN COUNT(c) > 0 THEN true ELSE false END FROM Customer c WHERE c.customer_staff_email = ?1")
+  boolean existsByCustomerStaffEmail(String customerStaffEmail);
 }
